f500f4ccd32dadd7e034026296314930f4f84bd1
Jonathan Gutiérrez <contacto@jgutierrez.cl>
2017-03-27 21:32:04 -0300

    Implement crowdin webhook

--- src/request/crowdin.js
+++ src/request/crowdin.js
@@ -1,4 +1,5 @@
 const config = require('../../config.json');
+const langs = require('./crowdin-langs.json');
 const log = require('../logger');
 module.exports = handler;
 
@@ -9,6 +10,15 @@ module.exports = handler;
  * @param {any} bot The Discord Client object
  */
 function handler(app, bot) {
+    // Crowdin webhooks
+    app.get('/crowdin/translated/:urlsecret/:channel/', check, (req, res) => {
+        receiver(false, req, res);
+    });
+
+    app.get('/crowdin/completed/:urlsecret/:channel/', check, (req, res) => {
+        receiver(true, req, res);
+    });
+
     function check(req, res, next) {
         let chanId = req.params.channel;
         let secret = req.params.urlsecret;
@@ -33,21 +43,68 @@ function handler(app, bot) {
             });
         }
 
+        if (!req.query.project) {
+            res.status(400);
+            return res.json({
+                success: false,
+                message: 'Project identifier not sent'
+            });
+        }
+
+        if (!req.query.language) {
+            res.status(400);
+            return res.json({
+                success: false,
+                message: 'Language not sent'
+            });
+        }
+
+        // Invalid project
+        if (!config.crowdinProjects.hasOwnProperty(req.query.project)) {
+            log.warn(`Received an invalid project`);
+            res.status(400);
+            return res.json({
+                success: false,
+                message: 'Invalid project'
+            });
+        }
+
+        // Unrecognized language
+        if(langs.hasOwnProperty(req.query.language)) {
+            log.warn(`Received an invalid language`);
+            res.status(400);
+            return res.json({
+                success: false,
+                message: 'Unrecognized language'
+            });
+        }
+
         next();
     }
 
-    // Crowdin webhooks
-    app.get('/crowdin/translated/:urlsecret/:channel/', check, (req, res, next) => {
-        res.json({
-            success: false,
-            message: 'WIP'
-        });
-    });
+    /**
+     * Receives the webhook request for both translated and completed requests
+     * 
+     * @param {boolean} completed Completed or translated (true or false respectively)
+     * @param {Express.Request} req Express request (yup, really)
+     * @param {Express.Response} res Express response (yes)
+     */
+    function receiver(completed, req, res) {
+        let projectName = config.crowdinProjects[req.query.project];
+        let langSplit = langs[req.query.language].split('|');
+        let message = 'Language ' + langSplit[0];
 
-    app.get('/crowdin/completed/:urlsecret/:channel/', check, (req, res, next) => {
-        res.json({
-            success: false,
-            message: 'WIP'
-        });
-    });
+        if (langSplit.length > 1) {
+            message += ` :${langSplit[1]}:`;
+        }
+
+        message += completed ? 
+            ' is now completed and verified!' :
+            ' is now fully translated! Time to proofread!';
+
+        let chan = bot.getChannel(req.params.channel);
+        chan.sendMessage(message);
+        
+        res.json({ success: true });
+    }
 }
