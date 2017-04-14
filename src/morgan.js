651707de1a1cb13fc973d9a3b0a7b639e62df63c
Jonathan Gutierrez <contacto@jgutierrez.cl>
2017-04-14 14:29:14 -0300

    Fix new morgan format

--- src/morgan.js
+++ src/morgan.js
@@ -1,9 +1,8 @@
 const morgan = require('morgan');
 const dateFormat = require('dateformat');
-// let lf = '[:date[clf]] :remote-addr :method :url :status ":referrer" ":user-agent"';
 
 module.exports = morgan(function (tokens, req, res) {
-    let dt = '[' + dateFormat(date, 'dd/mmm/yyyy:hh:MM:ss Z') + ']';
+    let dt = '[' + dateFormat((new Date()), 'dd/mmm/yyyy:hh:MM:ss Z') + ']';
     let ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
 
     return [
