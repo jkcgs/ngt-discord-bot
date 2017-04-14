35f455494431b3a976fbd6ff87551285b6f53dd5
Jonathan Gutierrez <contacto@jgutierrez.cl>
2017-04-14 14:34:20 -0300

    Change referrer format on log

--- src/morgan.js
+++ src/morgan.js
@@ -4,13 +4,14 @@ const dateFormat = require('dateformat');
 module.exports = morgan(function (tokens, req, res) {
     let dt = '[' + dateFormat((new Date()), 'dd/mmm/yyyy:hh:MM:ss Z') + ']';
     let ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+    let ref = tokens.referrer(req, res);
+    ref = ref ? '"' + tokens.referrer(req, res) + '"' : '""';
 
     return [
         dt, ip,
         tokens.method(req, res),
         tokens.url(req, res),
-        tokens.status(req, res),
-        '"' + tokens.referrer(req, res) + '"',
+        tokens.status(req, res), ref,
         '"' + req.headers['user-agent'] + '"',
         tokens.res(req, res, 'content-length'), '-',
         tokens['response-time'](req, res), 'ms'
